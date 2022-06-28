7a000354f065b44a96f003a3e68be2a2b7be28a2
vamseevk2001 <vamseevk2001@gmail.com>
2022-06-28 13:39:47 +0530

    sending mail done

--- app/src/main/java/com/example/sls/MainActivity.java
+++ app/src/main/java/com/example/sls/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
@@ -26,14 +27,17 @@ import com.example.sls.databinding.ActivityMainBinding;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Array;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE = 1;
     ActivityMainBinding binding;
     Boolean validated = false;
     private final static String TAG = "countryspinnerexample";
@@ -41,6 +45,17 @@ public class MainActivity extends AppCompatActivity {
     private AutoCompleteTextView countrySpinner;
     private AutoCompleteTextView stateSpinner;
     private AutoCompleteTextView citySpinner;
+
+    private static final int PICK_FROM_GALLERY = 101;
+    private static final int ADDRESS = 102;
+    private static final int DRIVING = 103;
+    private static final int AADHAAR = 104;
+    private static final int SIGNATURE = 105;
+    Uri aadhaarURI;
+    Uri addressURI;
+    Uri drivingURI;
+    Uri selfieURI;
+    Uri signatureURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,35 +75,74 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                validated = validate();
+                if (validated) {
+                StringBuilder mailBody = new StringBuilder();
+                mailBody.append("Name : " + binding.nameInp.getText().toString() + "\n");
+                mailBody.append("Company Name : " + binding.companyName.getText().toString() + "\n");
+                mailBody.append("GST no. : " + binding.gstNo.getText().toString() + "\n");
+                mailBody.append("Address : \n" + binding.address.getText().toString() + "\n");
+                mailBody.append("Mobile No. : " + binding.mobileNo.getText().toString() + "\n");
+                mailBody.append("Email id : " + binding.email.getText().toString() + "\n");
+                if (binding.purpose.getCheckedRadioButtonId() == R.id.Home)
+                    mailBody.append("purpose :" + " Home" + "\n");
+                else
+                    mailBody.append("purpose :" + " Office" + "\n");
+
+                if (binding.ott.getCheckedRadioButtonId() == R.id.ottYes)
+                    mailBody.append("OTT required : " + "Yes" + "\n");
+                else
+                    mailBody.append("OTT required : " + "No" + "\n");
+
+//                Intent email = new Intent(Intent.ACTION_SENDTO);
+//                email.setData(Uri.parse("mailto:"));
+//                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"vamseevk9390@gmail.com"});
+//                email.putExtra(Intent.EXTRA_SUBJECT, "New Smart Link");
+//                email.putExtra(Intent.EXTRA_TEXT, mailBody.toString());
+//                if (URI != null) {
+//                    email.putExtra(Intent.EXTRA_STREAM, URI);
+//                }
+//                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+//                Toast.makeText(getApplicationContext(), "submitted successfully !", Toast.LENGTH_SHORT).show();
+                final Intent emailIntent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+                //emailIntent.setData(Uri.parse("mailto:"));
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"vamseevk9390@gmail.com"});
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "New Smart List");
+
+                ArrayList<Uri> uris = new ArrayList<Uri>();
+                //convert from paths to Android friendly Parcelable Uri's
 
 
-//                validated = validate();
-//                if (validated) {
-                    StringBuilder mailBody = new StringBuilder();
-                    mailBody.append("Name : "+binding.nameInp.getText().toString()+"\n");
-                    mailBody.append("Company Name : "+binding.companyName.getText().toString()+"\n");
-                    mailBody.append("GST no. : "+binding.gstNo.getText().toString()+"\n");
-                    mailBody.append("Address : \n"+binding.address.getText().toString()+"\n");
-                    mailBody.append("Mobile No. : "+binding.mobileNo.getText().toString()+"\n");
-                    mailBody.append("Email id : "+binding.email.getText().toString()+"\n");
-                    if(binding.purpose.getCheckedRadioButtonId() == R.id.Home)
-                        mailBody.append("purpose :"+" Home"+"\n");
-                    else
-                        mailBody.append("purpose :"+" Office"+"\n");
+                if (aadhaarURI != null) {
+                    uris.add(aadhaarURI);
+                }
 
-                    if(binding.ott.getCheckedRadioButtonId() == R.id.ottYes)
-                        mailBody.append("OTT required : "+"Yes"+"\n");
-                    else
-                        mailBody.append("OTT required : "+"No"+"\n");
+                if (addressURI != null) {
+                    uris.add(addressURI);
+                    //emailIntent.putExtra(Intent.EXTRA_STREAM, addressURI);
+                }
 
-                    Intent email = new Intent(Intent.ACTION_SENDTO);
-                    email.setData(Uri.parse("mailto:"));
-                    email.putExtra(Intent.EXTRA_EMAIL, new String[]{"vamseevk9390@gmail.com"});
-                    email.putExtra(Intent.EXTRA_SUBJECT, "New Smart Link");
-                    email.putExtra(Intent.EXTRA_TEXT, mailBody.toString());
-                    startActivity(Intent.createChooser(email, "Choose an Email client :"));
-                    Toast.makeText(getApplicationContext(), "submitted successfully !", Toast.LENGTH_SHORT).show();
-                //}
+                if (drivingURI != null) {
+                    uris.add(drivingURI);
+//                    emailIntent.putExtra(Intent.EXTRA_STREAM, drivingURI);
+                }
+                if (selfieURI != null) {
+                    uris.add(selfieURI);
+//                    emailIntent.putExtra(Intent.EXTRA_STREAM, selfieURI);
+                }
+
+                if (signatureURI != null) {
+                    uris.add(signatureURI);
+
+                    emailIntent.putExtra(Intent.EXTRA_STREAM, signatureURI);
+                }
+
+
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, mailBody.toString());
+                emailIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+                startActivity(Intent.createChooser(emailIntent, "Sending email..."));
+                }
             }
         });
 
@@ -120,27 +174,27 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (binding.aadharCard.length() == 0) {
-            binding.aadharCard.setError("upload aadhaar card");
+            Toast.makeText(this, "upload aadhaar card", Toast.LENGTH_SHORT).show();
             return false;
         }
 
         if (binding.addressProof.length() == 0) {
-            binding.addressProof.setError("upload address proof");
+            Toast.makeText(this, "upload address proof", Toast.LENGTH_SHORT).show();
             return false;
         }
 
         if (binding.drivingLicense.length() == 0) {
-            binding.drivingLicense.setError("upload your driving license");
+            Toast.makeText(this, "upload your driving license", Toast.LENGTH_SHORT).show();
             return false;
         }
 
         if (binding.selfie.length() == 0) {
-            binding.selfie.setError("upload your selfie");
+            Toast.makeText(this, "upload your selfie", Toast.LENGTH_SHORT).show();
             return false;
         }
 
         if (binding.signature.length() == 0) {
-            binding.signature.setError("upload your Signature");
+            Toast.makeText(this, "upload your Signature", Toast.LENGTH_SHORT).show();
             return false;
         }
 
@@ -225,126 +279,122 @@ public class MainActivity extends AppCompatActivity {
         return json;
     }
 
-//    private fun selectImage() {
-//        val choice = arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Cancel")
-//        val myAlertDialog: AlertDialog.Builder = AlertDialog.Builder(this)
-//        myAlertDialog.setTitle("Select Image")
-//        myAlertDialog.setItems(choice, DialogInterface.OnClickListener { dialog, item ->
-//                when {
-//            // Select "Choose from Gallery" to pick image from gallery
-//            choice[item] == "Choose from Gallery" -> {
-//                val pickFromGallery = Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-//                pickFromGallery.type = "/image"
-//                startActivityForResult(pickFromGallery, 1)
+    void selectImage(int x) {
+//        String[] choice = new String[]{"Take Photo", "Choose from Gallery", "cancel"};
+//        final AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(this);
+//        final AlertDialog alertDialog = myAlertDialog.create();
+//        myAlertDialog.setTitle("Select Image");
+//        alertDialog.setCanceledOnTouchOutside(true);
+//        myAlertDialog.setItems(choice, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int item) {
+//                if (choice[item] == "Choose from Gallery") {
+        Intent pickFromGallery = new Intent();
+        pickFromGallery.setType("image/*");
+        pickFromGallery.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(pickFromGallery, "Select Picture"), x);
+//                }
+//                // Select "Take Photo" to take a photo
+//                else if (choice[item] == "Take Photo") {
+//                    Intent cameraPicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    startActivityForResult(cameraPicture, PICK_FROM_GALLERY);
+//                }
+//
+//                else if (choice[item] == "cancel") {
+//                    alertDialog.dismiss();
+//                }
+//
 //            }
-//            // Select "Take Photo" to take a photo
-//            choice[item] == "Take Photo" -> {
-//                val cameraPicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//                startActivityForResult(cameraPicture, 0)
-//            }
-//            // Select "Cancel" to cancel the task
-//            choice[item] == "Cancel" -> {
-//                myAlertDialog.dismiss()
-//            }
-//        }
-//        })
-//        myAlertDialog.show()
-//    }
-
-    void selectImage() {
-        String[] choice = new String[]{"Take Photo", "Choose from Gallery", "cancel"};
-        final AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(this);
-        final AlertDialog alertDialog = myAlertDialog.create();
-        myAlertDialog.setTitle("Select Image");
-        alertDialog.setCanceledOnTouchOutside(true);
-        myAlertDialog.setItems(choice, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int item) {
-                if (choice[item] == "Choose from Gallery") {
-                    Intent pickFromGallery = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    pickFromGallery.setType("/image");
-                    startActivityForResult(pickFromGallery, 1);
-                }
-                // Select "Take Photo" to take a photo
-                else if (choice[item] == "Take Photo") {
-                    Intent cameraPicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraPicture, 0);
-                }
-
-                else if (choice[item] == "cancel") {
-                    alertDialog.dismiss();
-                }
-
-            }
-        });
-        myAlertDialog.show();
+//        });
+//        myAlertDialog.show();
 
 
-    }
-
-    private void selectPdf() {
-        Intent pdfIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        pdfIntent.setType("application/pdf");
-        pdfIntent.addCategory(Intent.CATEGORY_OPENABLE);
-        startActivityForResult(pdfIntent, 12);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-//        if (resultCode != RESULT_CANCELED) {
-//            switch (requestCode) {
-//                case 0 :
-//                    if (resultCode == RESULT_OK && data != null) {
-//                    val imageSelected = data.extras!!["data"] as Bitmap?;
-//                    imageView.setImageBitmap(imageSelected);
-//                }
-//                case 1 :
-//                    if (resultCode == RESULT_OK && data != null) {
-//                    val imageSelected = data.data
-//                    val pathColumn = arrayOf(MediaStore.Images.Media.DATA)
-//                    if (imageSelected != null) {
-//                        val myCursor = contentResolver.query(
-//                                imageSelected,
-//                                pathColumn, null, null, null
-//                        )
-//                        if (myCursor != null) {
-//                            myCursor.moveToFirst()
-//                            val columnIndex = myCursor.getColumnIndex(pathColumn[0])
-//                            val picturePath = myCursor.getString(columnIndex)
-//                            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath))
-//                            myCursor.close()
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        // For loading PDF
-//        switch (requestCode) {
-//            case 12 :
-//                if (resultCode == RESULT_OK) {
-//                pdfUri = data?.data!!
-//                        val uri: Uri = data?.data!!
-//                        val uriString: String = uri.toString()
-//                var pdfName: String? = null
-//                if (uriString.startsWith("content://")) {
-//                    var myCursor: Cursor? = null
-//                    try {
-//                        myCursor = applicationContext!!.contentResolver.query(uri, null, null, null, null)
-//                        if (myCursor != null && myCursor.moveToFirst()) {
-//                            pdfName = myCursor.getString(myCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
-//                            pdfTextView.text = pdfName;
-//                        }
-//                    } finally {
-//                        myCursor?.close()
-//                    }
-//                }
-//            }
-//        }
+
+        if (requestCode == AADHAAR && resultCode == RESULT_OK) {
+            aadhaarURI = data.getData();
+            Cursor returnCursor =
+                    getContentResolver().query(aadhaarURI, null, null, null, null);
+
+            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            returnCursor.moveToFirst();
+            binding.aadharCard.setText(returnCursor.getString(nameIndex));
+        }
+
+        if (requestCode == ADDRESS && resultCode == RESULT_OK) {
+            addressURI = data.getData();
+            Cursor returnCursor =
+                    getContentResolver().query(addressURI, null, null, null, null);
+
+            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            returnCursor.moveToFirst();
+            binding.addressProof.setText(returnCursor.getString(nameIndex));
+        }
+
+        if (requestCode == DRIVING && resultCode == RESULT_OK) {
+            drivingURI = data.getData();
+            Cursor returnCursor =
+                    getContentResolver().query(drivingURI, null, null, null, null);
+
+            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            returnCursor.moveToFirst();
+            binding.drivingLicense.setText(returnCursor.getString(nameIndex));
+        }
+
+        if (requestCode == PICK_FROM_GALLERY && resultCode == RESULT_OK) {
+            selfieURI = data.getData();
+            Cursor returnCursor =
+                    getContentResolver().query(selfieURI, null, null, null, null);
+
+            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            returnCursor.moveToFirst();
+            binding.selfie.setText(returnCursor.getString(nameIndex));
+        }
+
+        if (requestCode == SIGNATURE && resultCode == RESULT_OK) {
+            signatureURI = data.getData();
+            Cursor returnCursor =
+                    getContentResolver().query(signatureURI, null, null, null, null);
+
+            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            returnCursor.moveToFirst();
+            binding.signature.setText(returnCursor.getString(nameIndex));
+        }
+
+
     }
 
 
+    public void uploadAadhar(View view) {
+        Intent pdfIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        pdfIntent.setType("application/pdf");
+        pdfIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(pdfIntent, AADHAAR);
+    }
 
+    public void uploadAddress(View view) {
+        Intent pdfIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        pdfIntent.setType("application/pdf");
+        pdfIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(pdfIntent, ADDRESS);
+    }
 
+    public void uploadDriving(View view) {
+        Intent pdfIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        pdfIntent.setType("application/pdf");
+        pdfIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(pdfIntent, DRIVING);
+    }
+
+    public void uploadSelfie(View view) {
+        selectImage(PICK_FROM_GALLERY);
+    }
+
+    public void uploadsignature(View view) {
+        selectImage(SIGNATURE);
+    }
 }
